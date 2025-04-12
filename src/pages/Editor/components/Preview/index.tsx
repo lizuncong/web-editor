cc024d54cded6fd025b08a972a4b09480b1dcd20
lizuncong <2900170896@qq.com>
2025-04-12 23:29:44 +0800

    feat: iframe src

--- src/pages/Editor/components/Preview/index.tsx
+++ src/pages/Editor/components/Preview/index.tsx
@@ -5,9 +5,12 @@ import { useAppSelector } from '@/store/hooks';
 import styles from './index.module.less';
 const Preview = memo(() => {
   const preview = useAppSelector((state) => state.editor.previewType);
+  const src = location.host.includes('localhost')
+    ? 'http://localhost:5174/'
+    : 'https://lizuncong.github.io/arise-theme/';
   return (
     <div className={styles.preview}>
-      <iframe sandbox="" className={[styles.iframe, styles[preview]].join(' ')} src="https://www.baidu.com/"></iframe>
+      <iframe sandbox="" className={[styles.iframe, styles[preview]].join(' ')} src={src}></iframe>
     </div>
   );
 });
