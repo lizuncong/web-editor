b189a10feebd722a3099b6dff6de2642bdedcba9
lizuncong <2900170896@qq.com>
2025-04-12 14:09:28 +0800

    feat: hash路由

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,11 @@
 import { lazy } from 'react';
-import { createBrowserRouter } from 'react-router';
+import { createHashRouter } from 'react-router';
 
 import ROUTE_PATH from './constant';
 const Home = lazy(() => import(/* webpackChunkName: 'home' */ '../pages/Home'));
 const Editor = lazy(() => import(/* webpackChunkName: 'dashboard' */ '../pages/Editor'));
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   { index: true, Component: Home },
   {
     Component: Editor,
