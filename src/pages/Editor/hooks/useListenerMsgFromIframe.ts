1ebeb72383e7f92aa6cb0e2c514dd86106246ae7
lizuncong <2900170896@qq.com>
2025-04-13 15:50:19 +0800

    feat: 删除console

--- src/pages/Editor/hooks/useListenerMsgFromIframe.ts
+++ src/pages/Editor/hooks/useListenerMsgFromIframe.ts
@@ -21,7 +21,6 @@ export const useListenerMsgFromIframe = () => {
     // 监听section schema配置项协议改变的信息
     const offAllSectionSchemaChange = iframeCommunicator.onAllSectionSchemaChange(
       (data: Record<string, SectionSchemaStruct>) => {
-        console.log('编辑器：onAllSectionSchemaChange', data);
         dispatch(
           changeEditorState({
             allSectionSchema: data,
@@ -31,23 +30,17 @@ export const useListenerMsgFromIframe = () => {
     );
     // 监听整个section config data改变
     const offSectionConfigData = iframeCommunicator.onSectionConfigData((data: SectionConfigDataStruct) => {
-      console.log('编辑器：onSectionConfigData', data);
-
       updateAllSectionConfigData(data, false);
     });
 
     // 监听sectionConfigData的order发生的改变
     const offSectionConfigOrderChange = iframeCommunicator.onSectionConfigOrderChange((order: string[]) => {
-      console.log('编辑器：onSectionConfigOrderChange', order);
-
       updateSectionConfigOrder(order, false);
     });
 
     // 监听sectionConfigData的某个section发生了改变
     const offSectionConfigSectionChange = iframeCommunicator.onSectionConfigSectionChange(
       ({ sectionId, section }: { sectionId: string; section: SectionConfigSchema }) => {
-        console.log('编辑器：onSectionConfigSectionChange', { sectionId, section });
-
         updateSectionConfigSectionBySectionId(sectionId, section, false);
       },
     );
@@ -55,16 +48,12 @@ export const useListenerMsgFromIframe = () => {
     // 监听sectionConfigData的sections发生的改变
     const offSectionConfigSectionsChange = iframeCommunicator.onSectionConfigSectionsChange(
       (data: Record<string, SectionConfigSchema | undefined>) => {
-        console.log('编辑器：onSectionConfigSectionsChange', data);
-
         updateSectionConfigSections(data, false);
       },
     );
     // 监听section schema多语言语料改变的消息
     const offSectionSchemaLanguage = iframeCommunicator.onSectionSchemaLanguage(
       (data: { i18nKey: string; locales: any }) => {
-        console.log('编辑器：onSectionSchemaLanguage', data);
-
         i18n.addResourceBundle(data.i18nKey, defaultNs, data.locales, true, true);
       },
     );
