dba272600ef8e15b6c8403e28561eba2755a434e
lizuncong <2900170896@qq.com>
2025-04-15 10:06:58 +0000

    Deploying to gh-pages from @ lizuncong/web-editor@5ef663c6d2348b476c4876c7a92b895030a48a6e 🚀

---
+++ assets/index-B16ESrci.js
@@ -0,0 +1,1 @@
+import{r as s,j as t}from"./index-CQRfQbsL.js";import{C as l}from"./index-D4lM_fJ_.js";import{u as a}from"./useTranslation-CxQRvK1M.js";const u=s.memo(e=>{const{value:r,setting:o,onChange:i}=e,{t:n}=a();return t.jsx(l,{label:o.label,info:o.info,children:t.jsx("div",{onClick:()=>{i({value:r})},children:n("editor.collectionpicker")})})});export{u as default};
--- assets/index-BZD80TqS.js
+++
@@ -1,1 +0,0 @@
-import{r as t,j as e}from"./index-5YH52zMZ.js";const n="_container_13la1_1",r={container:n},o=t.memo(()=>e.jsx("div",{className:r.container,children:"主题设置"}));export{o as default};
