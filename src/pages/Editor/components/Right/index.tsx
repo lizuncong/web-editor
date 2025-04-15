a8af63a910e18b8339340d35def7bc71b39741c0
lizuncong <lizuncong@huya.com>
2025-04-15 13:43:14 +0800

    feat: 输入框空间开发完成

--- src/pages/Editor/components/Right/index.tsx
+++ src/pages/Editor/components/Right/index.tsx
@@ -2,8 +2,9 @@ import { memo, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { useAppSelector } from '@/store/hooks';
-import { SettingsValue } from '@/types/section';
+import { SectionConfigSchema, SettingsValue } from '@/types/section';
 
+import { useUpdateConfigData } from '../../hooks/useUpdateConfigDataAndNotify';
 import Form from '../Form';
 import styles from './index.module.less';
 const Right = memo(() => {
@@ -11,11 +12,23 @@ const Right = memo(() => {
   const sectionConfigData = useAppSelector((state) => state.editor.sectionConfigData);
   const allSectionSchema = useAppSelector((state) => state.editor.allSectionSchema);
   const sectionSchema = allSectionSchema[currentEditingForm!.type];
-  const sectionConfig = sectionConfigData.sections[currentEditingForm!.sectionId];
+  const sectionConfig = sectionConfigData.sections[currentEditingForm!.sectionId]!;
+  const { updateSectionConfigSectionBySectionId } = useUpdateConfigData();
   const { t } = useTranslation();
-  const onSettingValueChange = useCallback((newSettingValue: SettingsValue) => {
-    console.log('newSettingValue...', newSettingValue);
-  }, []);
+  const onSettingValueChange = useCallback(
+    (newSettingValue: SettingsValue) => {
+      console.log('newSettingValue...', newSettingValue);
+      const newSectionConfigData: SectionConfigSchema = {
+        ...sectionConfig,
+        settingsData: {
+          ...sectionConfig.settingsData,
+          settings: newSettingValue,
+        },
+      };
+      updateSectionConfigSectionBySectionId(currentEditingForm!.sectionId, newSectionConfigData);
+    },
+    [currentEditingForm, sectionConfig, updateSectionConfigSectionBySectionId],
+  );
 
   return (
     <div className={styles.container}>
@@ -23,7 +36,7 @@ const Right = memo(() => {
       <div className={styles.list}>
         <Form
           settings={sectionSchema.settings ?? []}
-          settingValue={sectionConfig?.settingsData.settings ?? {}}
+          settingValue={sectionConfig.settingsData.settings ?? {}}
           onSettingValueChange={onSettingValueChange}
         />
       </div>
