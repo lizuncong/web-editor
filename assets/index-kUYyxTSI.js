1bc43c75cf253d05c75fe1520de0169db41300a5
lizuncong <2900170896@qq.com>
2025-04-19 06:40:49 +0000

    Deploying to gh-pages from @ lizuncong/web-editor@fdb1a2f7228a151b5ec4d46270e2f61f0e1ba173 🚀

---
+++ assets/index-kUYyxTSI.js
@@ -0,0 +1,1 @@
+import{r as s,j as t}from"./index-Bricc4_s.js";import{C as l}from"./index-gLXAw-EZ.js";import{u as a}from"./constant-CT33xb8z.js";const u=s.memo(e=>{const{value:r,setting:o,onChange:i}=e,{t:n}=a();return t.jsx(l,{label:o.label,info:o.info,children:t.jsx("div",{onClick:()=>{i({value:r})},children:n("editor.collectionpicker")})})});export{u as default};
